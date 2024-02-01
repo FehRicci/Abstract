3a087f99b8da7779fb4047aa9fe3203b041bdba7
Fernanda Ricci Braz <fernanda.ricci.braz@gmail.com>
2024-01-31 21:55:33 -0300

    Feitas as classes pessoa, pessoa fisica e pessoa juridica

---
+++ Abstract/Entities/PessoaFisica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.Entities
+{
+    internal class PessoaFisica : Pessoa
+    {
+        public double GastosSaude { get; set; }
+
+        public PessoaFisica(double gastosSaude, string nome, double rendaAnual) : base(nome, rendaAnual)
+        {
+            GastosSaude = gastosSaude;
+        }
+       
+        public override double CalculoImposto()
+        {
+            double abatimento = GastosSaude * 0.50;
+            if (RendaAnual < 20000)
+            {
+                return (RendaAnual * 0.15) - abatimento;
+            }
+            else
+            {
+                return (RendaAnual * 0.25) - abatimento;
+            }
+        }
+    }
+}
---
+++ Abstract/Entities/PessoaJuridica.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract.Entities
+{
+    internal class PessoaJuridica : Pessoa
+    {
+        public int NumFuncionarios { get; set; }
+
+        public PessoaJuridica(int numFuncionarios, string nome, double rendaAnual) : base(nome, rendaAnual)
+        {
+            NumFuncionarios = numFuncionarios;
+        }
+
+        public override double CalculoImposto()
+        {
+           if( NumFuncionarios > 10) 
+            {
+                return RendaAnual * 0.14;
+            }
+            else
+            {
+                return RendaAnual * 0.16;
+            }
+        }
+    }
+}
